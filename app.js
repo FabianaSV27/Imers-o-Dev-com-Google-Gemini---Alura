3fc250e75fd6cfcc58e5ef8e0e6135ec89b1ef03
Fabs san <fabianagithub@gmail.com>
2024-09-05 16:40:33 +0000

    aula 3

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log("Genshin Impect")
+console.log(dados);
+  
+ 
---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo: "Genshin Impact",
+        descricao: "Genshin Impact é um jogo de RPG de ação gratuito, ambientado em um mundo de fantasia chamado Teyvat. Ele conquistou milhões de jogadores ao redor do mundo com sua jogabilidade envolvente, gráficos deslumbrantes e história rica.",
+        caracteristicas:"Mundo aberto vibrante: Teyvat é um mundo vasto e cheio de vida, com paisagens diversas, desde montanhas nevadas até florestas exuberantes.\n\nSistema de elementos: O combate é baseado em um sistema de elementos que reage entre si, permitindo criar combos e estratégias únicas.\n\nExploração profunda: Além de explorar o mundo aberto, você pode escalar montanhas, nadar, planar e desvendar diversos mistérios.\n\nPersonagens carismáticos: Genshin Impact possui um elenco diversificado de personagens, cada um com sua própria história e habilidades.\n\nMultiplayer cooperativo: Possibilidade de jogar com amigos e enfrentar desafios juntos.\n\nHistória em constante evolução: A história principal é extensa e complexa, com atualizações regulares que adicionam novos capítulos e personagens.",
+        link:"https://genshin.hoyoverse.com/pt/home?utm_source=HoYoverse&utm_medium=products"
+    },
+    {
+        titulo: "Honkai Impact 3rd",
+        descricao: "Honkai Impact 3rd é um popular jogo de ação e RPG desenvolvido pela miHoYo. Ele se passa em um mundo pós-apocalíptico onde a humanidade enfrenta uma ameaça cósmica conhecida como Honkai. Os jogadores assumem o papel de valquírias, guerreiras poderosas com habilidades especiais, para combater o Honkai e proteger a Terra.",
+        caracteristicas:"Ação frenética: Combates rápidos e dinâmicos com combos elaborados e habilidades especiais.\n\nVisuais espetaculares: Gráficos 3D de alta qualidade, animações fluidas e modelos de personagens detalhados.\n\nVariedade de personagens: Um vasto elenco de valquírias com habilidades e estilos de luta únicos.\n\nHistória profunda: Uma narrativa envolvente que se aprofunda no universo de Honkai e nos personagens.\n\nMultiplayer cooperativo: Possibilidade de jogar com amigos e enfrentar desafios juntos.\n\nFoco em ação e combate: A mecânica de jogo gira em torno de combates intensos e estratégicos.",
+        link:"https://honkaiimpact3.hoyoverse.com/global/en-us/fab"
+    },
+    {
+        titulo: "Zenless Zone Zero",
+        descricao: "Zenless Zone Zero é o mais recente lançamento da HoYoverse, a mesma empresa por trás de sucessos como Genshin Impact e Honkai: Star Rail. Este novo título nos leva a um mundo urbano futurista, onde a humanidade luta contra criaturas estranhas em um ambiente pós-apocalíptico.",
+        caracteristicas:"Metrópole pós-apocalíptica: New Eridu é uma cidade futurista e vibrante, mas também perigosa, com criaturas estranhas e desafios urbanos.\n\nCombate rápido e frenético: O combate é mais focado em ação rápida, com combos e habilidades especiais.\n\nExploração urbana: Você explorará a cidade usando parkour, escalando edifícios e desvendando seus segredos.\n\nEstilo visual único: O jogo possui um estilo visual vibrante e colorido, com um design de personagens charmoso.\n\nHistória intrigante: A história se concentra em um grupo de sobreviventes que lutam contra as forças que ameaçam a cidade.",
+        link:"https://zenless.hoyoverse.com/pt-pt/main?utm_source=web&utm_medium=landingpage&utm_campaign=backcover"
+    },
+    {
+        titulo: "Honkai: Star Rail",
+        descricao: "Honkai: Star Rail é um jogo de RPG de aventura espacial desenvolvido pela HoYoverse, a mesma empresa por trás do popular Genshin Impact. O jogo se passa em um futuro distante, onde os jogadores embarcam em uma jornada galáctica a bordo do Expresso Astral.",
+        caracteristicas:"Combate por turnos: Um sistema de batalha mais estratégico, onde as ações são planejadas com antecedência.\n\nExploração espacial: Uma jornada por diversos planetas e sistemas solares, cada um com suas características únicas.\n\nElementos de RPG: Progressão dos personagens, missões secundárias e um mundo aberto para explorar.\n\nHistória rica: Uma narrativa envolvente que se expande ao longo do jogo.\n\nFoco em narrativa e exploração: Além do combate, o jogo oferece uma experiência mais rica em termos de história e exploração",
+        link:"https://hsr.hoyoverse.com/en-us/?utm_source=HoYoverse&utm_medium=products"
+    }
+]; //colocar [] para fazer lista
+
+
+
